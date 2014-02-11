06c30b06cf0cbb88e2b98a15f1cc44734c503400
fritzvd <f.vandeventer@gmail.com>
2014-02-11 08:39:27 +0100

    bg.. fugly

--- src/scenes/GameScene.hx
+++ src/scenes/GameScene.hx
@@ -1,6 +1,8 @@
 package scenes;
 
 import com.haxepunk.Scene;
+import com.haxepunk.graphics.Image;
+import com.haxepunk.Entity;
 import entities.Penguin;
 
 class GameScene extends Scene
@@ -12,6 +14,14 @@ class GameScene extends Scene
 
 	public override function begin()
 	{
+
+		var bitmap:Image = new Image("graphics/bg.png");
+        bitmap.x = - bitmap.width / 2;
+        bitmap.y = - bitmap.height / 2;
+        var bgEntity:Entity = new Entity(0,0,bitmap);
+        bgEntity.x =  (bitmap.width/2);
+        bgEntity.y =  (bitmap.height/2);
+        add(bgEntity);
 		add(new Penguin(200, 50));
 	}
 }
